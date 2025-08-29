0a7e779289e05d9d3a32267af5033761f1290ac7
zhou-yu <zhouyuo995@gmail.com>
2025-08-29 23:07:24 +0700

    theme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,23 @@
 import useCommonStore from "@/stores/useCommonStore";
 import tamaguiConfig from "@/tamagui.config"; // 你的 Tamagui 配置文件
-import { TamaguiProvider } from "@tamagui/core";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import "react-native-reanimated";
+import { TamaguiProvider } from "tamagui";
+import "../tamagui-web.css";
 import SplashScreen from "./splash";
 
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useColorScheme } from "react-native";
+
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
   const commonStore = useCommonStore();
 
   const [loaded] = useFonts({
@@ -23,9 +32,13 @@ export default function RootLayout() {
   if (commonStore.isSplash) {
     return (
       <GestureHandlerRootView>
-        <TamaguiProvider config={tamaguiConfig}>
-          <SplashScreen />
-          <StatusBar style="auto" />
+        <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme!}>
+          <ThemeProvider
+            value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+          >
+            <SplashScreen />
+            <StatusBar style="auto" />
+          </ThemeProvider>
         </TamaguiProvider>
       </GestureHandlerRootView>
     );
@@ -33,12 +46,16 @@ export default function RootLayout() {
 
   return (
     <GestureHandlerRootView>
-      <TamaguiProvider config={tamaguiConfig}>
+      <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme!}>
+         <ThemeProvider
+            value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+          >
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="+not-found" />
         </Stack>
         <StatusBar style="auto" />
+        </ThemeProvider>
       </TamaguiProvider>
     </GestureHandlerRootView>
   );
